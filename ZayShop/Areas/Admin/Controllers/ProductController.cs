553414028d80dd6f9f2ddedafddb67328fe100b4
unknown <you@example.com>
2024-11-06 13:00:16 +0400

    product update and delete actions makeing

--- ZayShop/Areas/Admin/Controllers/ProductController.cs
+++ ZayShop/Areas/Admin/Controllers/ProductController.cs
@@ -79,6 +79,77 @@ namespace ZayShop.Areas.Admin.Controllers
           return RedirectToAction(nameof(Index));
         }
 
+        #endregion
+        #region Delete
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product is null) return NotFound();
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+         
+            return RedirectToAction(nameof(Index));
+        }
 		#endregion
-	}
+
+		#region Update
+		[HttpGet]
+        public IActionResult Update(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product is null) return NotFound();
+            var model = new ProductUpdateVM
+            {
+                Title= product.Title,
+                 PhotoPath= product.PhotoPath,
+                 Size = product.Size,
+                 Price = product.Price,
+                 CategoryId = product.CategoryId,
+                 Categories=_context.Categories.Select(c => new SelectListItem
+                 {
+                     Text = c.Name,
+                      Value=c.Id.ToString()
+                 })
+                 .ToList()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(int id,ProductUpdateVM model)
+        { 
+            model.Categories=_context.Categories.Select(c=> new SelectListItem
+            {
+              Text= c.Name,
+              Value=c.Id.ToString()
+            }).ToList();
+
+            var product = _context.Products.Find(id);
+            if (product is null) return NotFound();
+            var existProduct=_context.Products.Any(p => p.Title == model.Title &&p.Id!=product.Id);
+            if (existProduct)
+            {
+                ModelState.AddModelError("Title", "Already exists");
+                return View(model);
+            }
+            var category=_context.Categories.Find(model.CategoryId);
+            if (category is null)
+            {
+                ModelState.AddModelError("Title","Not Found Category");
+                return View(model);
+            }
+            product.Title = model.Title;
+            product.PhotoPath = model.PhotoPath;
+            product.Size = model.Size;
+            product.Price = model.Price;
+            product.CategoryId = category.Id;
+            product.ModifiedAt= DateTime.Now;
+            _context.Products.Update(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+		#endregion
+	
+    }
 }
--- ZayShop/obj/Debug/net8.0/ZayShop.AssemblyInfo.cs
+++ ZayShop/obj/Debug/net8.0/ZayShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZayShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fa621c432723fb44af8c90cbf36f747c1c93002")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZayShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZayShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
